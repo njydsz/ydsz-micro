13b8afb25c20fe7a6ef66437d6925382eaf3e7ec
Marvin Lee <limw1888@126.com>
2026-09-19 22:31:32 +0800

    refactor(captcha): 调整验证码组件导入路径
    - 修改点选验证码组件导入为具体文件路径
    - 修改滑块旋转验证码导入为具体文件路径
    - 修改滑块平移验证码导入为具体文件路径
    - 保持滑块验证码导入路径不变
    - 导出类型声明未做修改

--- comm/effects/common-ui/src/components/captcha/index.ts
+++ comm/effects/common-ui/src/components/captcha/index.ts
@@ -5,10 +5,10 @@
  * @author ydsz-team
  * @since 1.0.0
  */
-export { default as YdPointSelectionCaptcha } from './point-selection-captcha/index.vue';
-export { default as PointSelectionCaptchaCard } from './point-selection-captcha/index.vue';
+export { default as YdPointSelectionCaptcha } from './point-selection-captcha/YdPointSelectionCaptcha.vue';
+export { default as PointSelectionCaptchaCard } from './point-selection-captcha/YdPointSelectionCaptchaCard.vue';
 
 export { default as YdSliderCaptcha } from './slider-captcha/index.vue';
-export { default as SliderRotateCaptcha } from './slider-rotate-captcha/index.vue';
-export { default as SliderTranslateCaptcha } from './slider-translate-captcha/index.vue';
+export { default as SliderRotateCaptcha } from './slider-rotate-captcha/YdSliderRotateCaptcha.vue';
+export { default as SliderTranslateCaptcha } from './slider-translate-captcha/YdSliderTranslateCaptcha.vue';
 export type * from './types';
